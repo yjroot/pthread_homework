52fef1e852b7d5fec21e449d3404fef0d560f1e1
YeonJun Lim <yjroot@gmail.com>
2013-06-06 14:38:28 +0900

    Implemented code that measure execution time

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/time.h>
 
 #define N 100000000  //10^8
 double S[N], A[N], B[N];
+
+static inline long get_time_ms() {
+    struct timeval tv;
+    gettimeofday (&tv, NULL);
+    return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
 
 int main(void) {
     int i;
@@ -14,9 +21,12 @@ int main(void) {
         B[i] = i+2;
         S[i] = 0;
     }
+
+    long starttime = get_time_ms();
     // main computation
     for (i=0; i<N; i++)
         S[i] = log(A[i]) + log(B[i]);
+    long endtime = get_time_ms();
 
     // accumulation of S[]
     double sum = 0;
@@ -25,6 +35,7 @@ int main(void) {
 
     // printing out the final sum
     printf("sum = %lf\n", sum);
+    printf("execution time = %ld\n", endtime-starttime);
 
     return 0;
 }
